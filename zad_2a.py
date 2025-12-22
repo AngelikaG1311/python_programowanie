be359e629cdc0786120fac899f07f5a56f8bb04a
ag <guzik.angelika@gmail.com>
2025-12-22 15:29:14 +0100

    branch python_basics zgodny z pep8

---
+++ zad_2a.py
@@ -0,0 +1,7 @@
+def wyswietl_imiona(lista_imion):
+    for imie in lista_imion:
+        print(imie)
+
+
+imiona = ['Jan', 'Janek', 'Jasiu', 'Janko', 'Janeczek']
+wyswietl_imiona(imiona)
---
+++ zad_2c.py
@@ -0,0 +1,8 @@
+def parzysta(lista_liczb):
+    for liczba in lista_liczb:
+        if liczba % 2 == 0:
+            print(liczba)
+
+
+lista = list(range(1, 11))
+parzysta(lista)
